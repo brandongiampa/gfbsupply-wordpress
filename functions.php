08c103ff09fe29ac2677c378243cbdc0cc7a0f68
Brandon Giampa <brandongiampa555@gmail.com>
2020-10-27 20:24:35 -0700

    add & require navwalker class, make menu descriptions translatable

--- functions.php
+++ functions.php
@@ -1,10 +1,18 @@
 <?php
 
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
 register_nav_menus( array(
-    'gfbs_main_menu' => 'GFB Supply Main Menu',
-    'gfbs_wc_product_category_menu' => 'GFB Supply WooCommerce Product Category Menu',
-    'gfbs_logged_in_menu' => 'GFB Supply User Logged In Menu',
-    'gfbs_not_logged_in_menu' => 'GFB Supply User NOT Logged In Menu'    
+    'gfbs_main_menu' => __( 'GFB Supply Main Menu', 'gfb_supply' ),
+    'gfbs_wc_product_category_menu' => __( 'GFB Supply WooCommerce Product Category Menu', 'gfb_supply' ),
+    'gfbs_logged_in_menu' => __( 'GFB Supply User Logged In Menu', 'gfb_supply' ),
+    'gfbs_not_logged_in_menu' => __( 'GFB Supply User NOT Logged In Menu', 'gfb_supply' )   
 ));
 
 ?>
