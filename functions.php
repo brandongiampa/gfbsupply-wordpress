4ef293481b7c01ae50de0cff9b7687838a0cc3f5
Brandon Giampa <brandongiampa555@gmail.com>
2020-10-27 20:14:25 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+register_nav_menus( array(
+    'gfbs_main_menu' => 'GFB Supply Main Menu',
+    'gfbs_wc_product_category_menu' => 'GFB Supply WooCommerce Product Category Menu',
+    'gfbs_logged_in_menu' => 'GFB Supply User Logged In Menu',
+    'gfbs_not_logged_in_menu' => 'GFB Supply User NOT Logged In Menu'    
+));
+
+?>
+
